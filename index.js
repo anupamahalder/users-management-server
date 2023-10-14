3f53a24fc850f14fcb216749b1819786c5d676ba
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-15 00:10:19 +0530

    Added cors middleware to give error free data access using api call from client side

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 // to avoid any issue regarding port after deployment 
 const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
 
 //here we can create some hard coded data 
 const users=[
